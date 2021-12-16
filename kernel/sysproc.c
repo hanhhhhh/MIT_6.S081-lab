035353e1668063c1c9bf558b60b16177027a66cc
hanhhh <1415231703@qq.com>
2021-12-16 09:43:57 +0800

    lab3/detecting which pages have been accessed/new

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -77,12 +77,8 @@ sys_sleep(void)
 
 
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
-{
-  // lab pgtbl: your code here.
-  return 0;
-}
+extern pte_t * walk(pagetable_t,uint64,int); // trampoline.S
+
 #endif
 
 uint64
@@ -106,4 +102,53 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+
+
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  //arg1:要检查的第一个用户页的起始虚地址
+  //arg2:要检查的页面number
+  //arg3:指向缓存的用户地址，来把结果存到一个bitmask，每页使用一位的数据结构，其中第一页对应于最低有效位
+  uint64 start_va;  
+  if(argaddr(0, &start_va) < 0)
+    return -1;
+  int number;
+  if(argint(1, &number) < 0)
+    return -1;
+  uint64 ua;
+  if(argaddr(2, &ua) < 0)
+    return -1;
+  uint64 resultmask=1;
+  uint64 bitmask=0;
+  uint64 PTE_A_mask=~PTE_A;
+  if(number>64){
+    printf("panic! number of page over the upper limit");
+    return -1;
+  }
+  uint64 va=start_va;
+  pte_t *pte;
+  for(int i=0;i<number;i++){
+    pte=walk(myproc()->pagetable,va,0);
+        //判断是否访问过  
+    // if(((*pte)&PTE_A)==0) continue;
+    // else{
+    //   printf("hello\n");
+    //   *pte=*pte & PTE_A_mask;   //访问过的clear PTE_A
+    //   bitmask=bitmask|(resultmask<<i);  
+    //   // printf("bit:%p\n",bitmask); 
+    // }
+    if((*pte)&PTE_A){    //访问过
+      // printf("hello\n");
+      *pte=*pte & PTE_A_mask;   //访问过的clear PTE_A
+      bitmask=bitmask|(resultmask<<i);  
+      // printf("bit:%p\n",bitmask); 
+    }
+    va+=PGSIZE;
+  }
+  copyout(myproc()->pagetable,ua,(char *)&bitmask,sizeof(uint64));
+  return 0;
 }
